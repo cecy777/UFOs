30751803c6340eec2a52f1919555cf564b0acbb2
cecy777 <cecyramram32@gmail.com>
2022-09-01 11:06:33 -0600

    adding Files

--- test.js
+++
@@ -1,8 +0,0 @@
-let vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
-
-
-for (var i = 0; i < vegetables.length; i++) {
-    console.log("I love " + vegetables[i]);
-}
-
-listLoops(vegetables) 
